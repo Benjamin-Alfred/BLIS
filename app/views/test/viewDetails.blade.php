51aa3bc40b15de2442ce5c879c07bc8e11a553de
Thomas Mapesa <mapesa@gmail.com>
2020-12-28 18:35:58 +0300

    Bug fix: Show test summary even if creator/performer/verifier is deleted

--- app/views/test/viewDetails.blade.php
+++ app/views/test/viewDetails.blade.php
@@ -89,14 +89,14 @@
 									{{ $test->visit->visit_type }}
 								@endif</p>
 							<p class="view-striped"><strong>{{trans('messages.registered-by')}}</strong>
-								{{ $test->created_by > 0 && $test->created_by != 2 ? $test->createdBy->name : trans('messages.unknown') }}</p>
+								{{ $test->created_by > 0 && $test->created_by != 2 ? $test->createdBy()->withTrashed()->first()->name : trans('messages.unknown') }}</p>
 
 							<p class="view"><strong>{{trans('messages.tested-by')}}</strong>
-								{{ $test->tested_by > 0 ? $test->testedBy->name : trans('messages.unknown') }} </p>
+								{{ $test->tested_by > 0 ? $test->testedBy()->withTrashed()->first()->name : trans('messages.unknown') }} </p>
 
 							@if($test->isVerified())
 								<p class="view"><strong>{{trans('messages.verified-by')}}</strong>
-									{{ $test->verified_by > 0 ? $test->verifiedBy->name : trans('messages.verification-pending') }} </p>
+									{{ $test->verified_by > 0 ? $test->verifiedBy()->withTrashed()->first()->name : trans('messages.verification-pending') }} </p>
 							@endif
 
 							@if((!$test->specimen->isRejected()) && ($test->isCompleted() || $test->isVerified()))
