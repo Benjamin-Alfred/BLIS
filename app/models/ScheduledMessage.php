2fd944f6d00e0f8a7949e44dc14192d61ad733b9
Thomas Mapesa <mapesa@gmail.com>
2020-12-29 09:53:24 +0300

    Add location content to AMR messages

--- app/models/ScheduledMessage.php
+++ app/models/ScheduledMessage.php
@@ -10,27 +10,38 @@ class ScheduledMessage extends Eloquent
 	 */
 	protected $table = 'scheduled_messages';
 
+	public $timestamps = false;
+
 	public static function scheduleTestResponse($testID, $destinationURI = "http://microbiologyapi.nphl.go.ke/v1", $status = 0, $retries = 3, $retryInterval = 60, $timeToSend = "now()"){
 
 		$content['payload'] = "";
+		$requestRecord = ExternalDump::where('test_id', '=', $testID)->first();
+		$location = explode("|", $requestRecord->city);
+		$county = count($location)>0?$location[0]:"Bungoma";
+		$subCounty = count($location)>1?$location[1]:"";
+		$ward = count($location)>2?$location[2]:"";
+		$village = count($location)>3?$location[3]:"";
 		$test = Test::find($testID);
-		if($test){
+
+		$AMRTests = Config::get('kblis.amr-test-name-aliases');
+
+		if($test && in_array(strtoupper($test->testType->name), $AMRTests)){
 			$testContent = [];
 			$testContent['patient_number'] = $test->visit->visit_number;
 			$testContent['gender'] = $test->visit->patient->getGender();
 			$testContent['age'] = $test->visit->patient->getAge("Y");
 			$testContent['age_unit'] = "years";
-			$testContent['county'] = "";
-			$testContent['sub_county'] = "";
-			$testContent['village'] = "";
-			$testContent['prediagnosis'] = "";
+			$testContent['county'] = $county;
+			$testContent['sub_county'] = $subCounty;
+			$testContent['village'] = $village;
+			$testContent['prediagnosis'] = $requestRecord->provisional_diagnosis;
 			$testContent['specimen_collection_date'] = $test->specimen->time_accepted;
-			$testContent['patient_type'] = $test->visit->visit_type;
-			$testContent['ward'] = "";
-			$testContent['admission_date'] = "";
-			$testContent['currently_on_therapy'] = "";
+			$testContent['patient_type'] = $requestRecord->patient_visit_type;
+			$testContent['ward'] = strcmp($requestRecord->patient_visit_type, "IP") == 0?$ward:"";
+			$testContent['admission_date'] = strcmp($requestRecord->patient_visit_type, "IP") == 0?$requestRecord->admission_date:"";
+			$testContent['currently_on_therapy'] = $requestRecord->request_notes;
 			$testContent['specimen_type'] = $test->specimen->specimenType->name;
-			$testContent['specimen_source'] = $test->specimen->specimenType->name;
+			$testContent['specimen_source'] = Config::get('kblis.organization');
 			$testContent['lab_id'] = Config::get('kblis.facility-code');
 
 			$testContent['isolates'] = $test->getCultureIsolates();
@@ -39,6 +50,8 @@ class ScheduledMessage extends Eloquent
 
 			$content['payload'] = $testContent;
 		}
+
+		Log::info("[ScheduledMessage] content = ".json_encode($content));
 
 		$scheduledMessage = new ScheduledMessage();
 		$scheduledMessage->payload = json_encode($content);
